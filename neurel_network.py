2f3111042b653c5c5540dbe58472c51d51f46af1
mebarkiyacine <mebarkiyacine822@gmail.com>
2026-01-31 14:50:26 +0100

    update my neural network

--- neurel_network.py
+++ neurel_network.py
@@ -17,13 +17,17 @@ def neural_network(x,y,learning_rate=0.01,n_layer=2,n_neurons=[5,5],epochs=1000)
     y_onehot=np.eye(n_classes)[y]
     dw=[]
     dp=[]
+    Z=[]
 
     for i in range(epochs):
 
         loss=0
+        
         for t in range(n_samples):
+            A=[]
+            Z=[]
             for j in range(n_layer):
-                if i==0:
+                if i==0 and t==0:
                     if j!=0:
                         n_features=n_neurons[j-1]
                     w=np.random.randn(n_features,n_neurons[j])*0.01
@@ -39,33 +43,54 @@ def neural_network(x,y,learning_rate=0.01,n_layer=2,n_neurons=[5,5],epochs=1000)
                     z=a_prev @ w +b
                     a=sigmoid(z)
                     a_prev=a
+                    Z.append(z)
+                    A.append(a)
+
                 else:
                 
                     
                     z=a_prev @ w +b
                     a=sigmoid(z)
                     a_prev=a
+                    Z.append(z)
                     A.append(a)
+            if i==0 and t==0:
+                w_out=np.random.randn(n_neurons[-1],n_classes)*0.01
+                b_out=np.zeros(n_classes)
+                       
                     
                
                 
-            z=a_prev @ wights[-1] + biases[-1]
+            z=a_prev @ w_out + b_out
             p=softmax(z)
+            
             dZ_out = p-y_onehot[t]
-            dW_out = a_prev.T @ dZ_out
-            db_out = np.sum(dZ_out, axis=0)
-            loss += -np.log(p[y[t]])
-            for l in reversed(range(n_layer)):
-                da=dZ_out[l+1]@ wights[l+1].T
-                dZ_out[l]=da * sigmoid_derivative(z)
-                dw[l] = np.outer(A[l], dZ_out[l])
-                dp[l] = dZ_out[l]
+            dw_out=np.outer(A[-1], dZ_out)
+            db_out=dZ_out
+            dA = dZ_out @ w_out.T
+
+            dw = [0]*n_layer
+            db = [0]*n_layer
 
             
-            for k in range(len(wights)) :
+            for l in reversed(range(n_layer)):
+                dZ = dA * sigmoid_derivative(Z[l])
+
+                if l==0:
+                    dw[l] = np.outer(x[t], dZ)
+                else:
+                    dw[l] = np.outer(A[l-1], dZ)
+
+                db[l] = dZ
+
+                dA = dZ @ wights[l].T
+            
+            for k in range(n_layer): 
                 wights[k]-=learning_rate*dw[k]
-                biases[k]-=learning_rate*dp[k]
-    return wights,biases
+                biases[k]-=learning_rate*db[k]
+            w_out-=learning_rate*dw_out
+            b_out-=learning_rate*db_out    
+    return wights,biases,w_out,b_out
         
                       
 
