e2d265c417601fd36afa8d51fed29b46157c62cb
mebarkiyacine <mebarkiyacine822@gmail.com>
2026-02-10 19:10:32 +0100

    adding embedding

--- rnn/tok.py
+++ rnn/tok.py
@@ -1,10 +1,12 @@
+import numpy as np
+
 def normalization(text):
     return text.lower().split()
 
 class tokenizer:
     def __init__(self):
         self.wordid={}
-        self.vecword=[]
+        self.vecword={}
         self.idx=1
     def fit(self,texts):
         for text in texts:
@@ -22,7 +24,26 @@ class tokenizer:
         for word in text:
             if word in self.wordid:
                 result.append(self.wordid[word])
-        return result        
+        return result
+    
+    
+    def embedding_tran(self,output_dim,input_dim):
+        for word in self.wordid:
+            self.vecword[self.wordid[word]]=np.random.rand(output_dim)*0.1
+    def embedding(self,vec_text):
+        emb_vec=[]
+        for text in vec_text:
+            vec=[]
+            for word in text:
+                if word in self.vecword:
+                    vec.append(self.vecword[word])
+            emb_vec.append(vec)
+        return emb_vec            
+                    
+                
+                
+            
+                    
 def padding(text_vec,maxlen):
     result=[]
     for vec in text_vec:
@@ -41,15 +62,19 @@ text_array = [
     "Deep learning is fun",
     "Hello",
     "Python is great for NLP",
-    "RNN LSTM Transformers"
+    "RNN LSTM Transformers",
+    "i love deep learning"
 ]
        
 tok=tokenizer()
 tok.fit(text_array)
 vec=[tok.encode(text) for text in text_array]
 print("tokenization",vec)
-vec_padd=padding(vec,10)
-print("the padding is",vec_padd)            
+vec_padd=padding(vec,5)
+print("the padding is",vec_padd)      
+tok.embedding_tran(4,5)
+result=tok.embedding(vec_padd)
+print(result)      
             
                                         
                     
