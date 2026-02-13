36fb362988aca7936e5d1bff60acd12c35ed7451
mebarkiyacine <mebarkiyacine822@gmail.com>
2026-02-13 17:39:43 +0100

    it works in little dataset

--- rnn/rnn.py
+++ rnn/rnn.py
@@ -31,29 +31,56 @@ class layer:
                 for l in self.layers:
                     if l.type=="embedding":
                         l.embedding_tran()
+                        
                         a=l.forward([a])
+                        a=a[-1]
                     else :
                         p=l    
                         a=l.forward(a) 
                        
                 
                 if epoch==0 and t==0:
-                    self.w_out=np.random.rand(a.size,n_class)
+                    self.w_out=np.random.rand(n_class,p.H_size)
                     self.b_out=np.zeros(n_class)
-                z=self.w_out @ p.hidden[-1]
+                z=self.w_out @ p.hidden[-1] +self.b_out
                 y_pred=np.tanh(z)
                 dz=y_pred-y_onehot[t]
                 dw=dz[:,None] @ p.hidden[-1][None,:]
+                dout=self.w_out.T @ dz
 
-                for l in reversed(range(len(self.layers))):
+                for i in reversed(range(len(self.layers))):
+                    l=self.layers[i]
                     if l.type=="recurente":
                         dh_next = np.zeros(l.H_size)
                         for h in reversed(range(len(l.hidden))):
-                            dh_next=l.backdrop(dz,learning_rate,x[t],dh_next,h)
+                            dh_next=l.backdrop(dout,learning_rate,x[t],dh_next,h)
            
             
                 self.w_out-=learning_rate*dw
-                self.b_out-=learning_rate*dz 
+                self.b_out-=learning_rate*dz
+    def predict(self,x):
+        x=np.array(x)
+        n_samples=x.shape[0]
+        result=[]
+        for i in range(n_samples):
+            a=x[i]
+            for l in self.layers:
+                
+                if l.type=="embedding":
+                    
+                    a=l.forward([a])
+                    a=a[-1]
+                else :
+                    p=l    
+                    a=l.forward(a)
+            z=self.w_out @ p.hidden[-1] +self.b_out
+            y_pred=np.tanh(z)
+            result.append(np.argmax(y_pred))
+        return np.array(result)    
+                     
+                        
+                
+                             
             
             
             
@@ -84,3 +111,11 @@ model.addrecun(6)
 model.fit(vec_padded,labels)
 print("wight",model.w_out)
 print("bias",model.b_out)
+text_pre=[
+    "i love python",
+    "i love machine learning"
+]
+vec_pre=tok.encode(text_pre)
+vec_pre=tok.padding(vec_pre,5)
+result=model.predict(vec_pre)
+print(result)
