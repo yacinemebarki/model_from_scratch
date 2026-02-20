b1a2c1bad6d9d7e438fbd7d9fff1afd2b986bc81
mebarkiyacine <mebarkiyacine822@gmail.com>
2026-02-20 15:32:56 +0100

    completing backprobagation in multi self attention

--- bert/multi_self_attention.py
+++ bert/multi_self_attention.py
@@ -11,7 +11,7 @@ class msa:
         self.w=None
         self.q=None
         self.v=None
-        dk=input_size/num_heads
+        dk=input_size//num_heads
         self.dk=dk
         self.input_size=input_size
         self.kw=np.random.rand(input_size,dk)*0.01
@@ -27,14 +27,29 @@ class msa:
         out=[]
         weight=[]
         arrayv=[]
+        outq=[]
+        arrayk=[]
         
         for i in range(len(x)):
             tok=masked[i]
             q=tok @ self.qw
             z=np.zeros(dk)
-            scores = np.array([q @ (masked[j] @ self.kw) / np.sqrt(dk) for j in range(len(x))])
+            outq.append(q)
+            
+            scores=[]
+            vec1=[]
+            
+            for j in range(len(masked)):
+                k_j = masked[j] @ self.kw
+                k=q@k_j/np.sqrt(dk)
+                vec1.append(k_j)
+                scores.append(k)
+            arrayk.append(k_j)    
+                
+            scores = np.array(scores)
             weights = softmax(scores)
             vec=[]
+            
             for j in range(len(x)):
                 
                 
@@ -46,24 +61,34 @@ class msa:
             weight.append(weights)
             
         return np.array(out),target,weight,arrayv
-    def backdrop(self,x,weights,z,arrayv):
+    def backdrop(self,x,weights,z,arrayv,arrayk,q,lr):
         dwv=np.zeros((self.input_size,self.dk))
         dwq=np.zeros((self.input_size,self.dk))
         dwk=np.zeros((self.input_size,self.dk))
+        dak = self.kw.shape[1]
         
-        dq=np.zeros_like(self.dk)
-        dk=np.zeros_like(self.dk)
-        da=np.zeros_like(len(weights))
+        dq=np.zeros(self.dk)
+        dv=np.zeros((self.input_size,self.dk))
+        
+        da=np.zeros(len(weights))
         
         
         
         for i in range(len(weights)):
-            dv+=weights[i]*z
-            dwv=np.outer(x[i],dv)
+            dv=weights[i]*z
+            dwv+=np.outer(x[i],dv)
             da[i]=z@arrayv[i]
         ds = weights*(da - np.sum(da * weights))
         for i in range(len(ds)):
-            dq+=ds[i]    
+            dq+=ds[i]*arrayk[i]/np.sqrt(self.dk) 
+            dk_j = ds[i] * q / np.sqrt(self.dk)
+            dwk += np.outer(x[i], dk_j)
+            dwq +=np.outer(x, dq)
+            
+        self.qw -= lr * dwq
+        self.kw -= lr * dwk
+        self.vw -= lr * dwv
+               
             
             
 
